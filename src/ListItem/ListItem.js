abaf2da044f8a3bda8ff6f3496859d6df3548786
egnica <46502133+egnica@users.noreply.github.com>
2022-07-05 21:29:08 -0500

    Add files via upload

--- src/ListItem/ListItem.js
+++ src/ListItem/ListItem.js
@@ -1,10 +1,18 @@
+import '../App.css'
+
 const ListItem = (props) => {
 console.log(props);
     return(
-        <div>
-        
-            {props.characterList.map((character) => <p key={character.id}>{character.name}</p>)}
-            
+        <div className = 'container'>
+            <div className = 'col-a'>
+                {props.characterList.map((character) => <p key={character.id}>{character.name}</p>)}
+            </div>
+            <div className = 'col-b'>
+                {props.characterList.map(character => <p key={character.id}>{character.height}</p>)}
+            </div>
+            <div className = 'col-c'>
+                {props.characterList.map(character => <p key={character.id}>{character.mass}</p>)}
+            </div>
         </div>
     )
 }
